cad846e80a305eb61008531a9cc4c1d8a8290adc
Anderson Domingues <ti.andersondomingues@gmail.com>
2024-05-29 20:47:18 -0300

    adicionados mais testes para a função soma

--- test_main.py
+++ test_main.py
@@ -4,5 +4,8 @@ from main import soma, subtracao
 def test_soma():
     assert soma(1, 2) == 3
 
+def test_soma_outro():
+    assert soma(soma(2,3), 5) == 10
+
 def test_subtracao():
     assert subtracao(1, 2) == -1
